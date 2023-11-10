d57fab1f53bc1d5038e57cfbce878410ae3ca4a3
Filip Jakovina <fj53514@fer.hr>
2023-11-10 17:13:35 +0100

    node.js

--- Web2-Lab2/ViewModels/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Web2_Lab2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
